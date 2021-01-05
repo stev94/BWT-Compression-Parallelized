7898297e5f93e19eba829c55864d09716c0c880b
Stefano Valladares <stefano.valladares12@gmail.com>
2021-01-06 00:42:33 +0100

    Setting up develop branch

--- cli/version.py
+++ cli/version.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-__version__ = '0.1.0'
+__version__ = '0.2.0-dev'
 PROGRAM_NAME = 'pbwt cli'
 
 venv_path = os.environ.get('VIRTUAL_ENV', os.path.join('..', 'venv'))
