a8a41c4c9aa8ca0f9ae9f16b23ba79e4f5989ad7
Stefano Valladares <stefano.valladares12@gmail.com>
2021-01-06 22:53:45 +0100

    Fixed ci

--- makefiles/cli.mk
+++ makefiles/cli.mk
@@ -2,6 +2,8 @@
 test-cli:									## Run python tests
 	python -c "import sys; print(sys.path)" && \
 	source $(CURDIR)/venv/bin/activate && \
+	pip list && \
+	python -c "import sys; print(sys.path)" && \
 	pytest $(CURDIR)/tests/test_pbwt.py::TestPBWT::test_run_tests $(CURDIR)/tests/test_cli.py
 
 .PHONY: make-venv
